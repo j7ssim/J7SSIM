ff2d9ead7be6b2e938e883de3b306a311791077f
j7ssim <nnvjokbcttt0@gmail.com>
2026-05-21 22:32:52 +0000

    Deploying to gh-pages from @ j7ssim/J7SSIM@fd5f2a8feec97f266e74c5a01b9578a36ab41f64 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "2485408834" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "1304123585" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
